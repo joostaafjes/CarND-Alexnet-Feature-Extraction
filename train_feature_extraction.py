913b84164f61808cde5ed986311f248245f1be89
joost aafjes <joostaafjes@gmail.com>
2018-03-18 22:42:27 +0100

    removed inference part

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -101,30 +101,4 @@ with tf.Session() as sess:
     saver.save(sess, './lenet')
     print("Model saved")
 
-#
-# Inference
-#
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)
 
-# Read Images
-im1 = imread("construction.jpg").astype(np.float32)
-im1 = im1 - np.mean(im1)
-
-im2 = imread("stop.jpg").astype(np.float32)
-im2 = im2 - np.mean(im2)
-
-# Run Inference
-t = time.time()
-output = sess.run(probs, feed_dict={x: [im1, im2]})
-
-# Print Output
-for input_im_ind in range(output.shape[0]):
-    inds = np.argsort(output)[input_im_ind, :]
-    print("Image", input_im_ind)
-    for i in range(5):
-        print("%s: %.3f" % (sign_names.ix[inds[-1 - i]][1], output[input_im_ind, inds[-1 - i]]))
-    print()
-
-print("Time: %.3f seconds" % (time.time() - t))

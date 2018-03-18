5d0e54543fd2fb8db8bd6eec70b4db85c5a69c8b
joost aafjes <joostaafjes@gmail.com>
2018-03-18 21:41:17 +0100

    add cifar10

---
+++ train_feature_extraction_cifar10.py
@@ -0,0 +1,104 @@
+import pickle
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from alexnet import AlexNet
+import time
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+import datetime
+from scipy.misc import imread
+from caffe_classes import class_names
+
+nb_classes = 10
+EPOCHS = 10
+BATCH_SIZE = 128
+
+from keras.datasets import cifar10
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+# y_train.shape is 2d, (50000, 1). While Keras is smart enough to handle this
+# it's a good idea to flatten the array.
+y_train = y_train.reshape(-1)
+y_test = y_test.reshape(-1)
+
+from sklearn.model_selection import train_test_split
+X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.3, random_state=42, stratify = y_train)
+
+# Define placeholders and resize operation.
+x = tf.placeholder(tf.float32, (None, 32, 32, 3))
+y = tf.placeholder(tf.int32, (None))
+resized = tf.image.resize_images(x, (227, 227))
+one_hot_y = tf.one_hot(y, nb_classes)
+
+# pass placeholder as first argument to `AlexNet`.
+fc7 = AlexNet(resized, feature_extract=True)
+# NOTE: `tf.stop_gradient` prevents the gradient from flowing backwards
+# past this point, keeping the weights before and up to `fc7` frozen.
+# This also makes training faster, less work to do!
+fc7 = tf.stop_gradient(fc7)
+
+# Add the final layer for traffic sign classification.
+shape = (fc7.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix
+
+# SOLUTION: Layer 8: Fully Connected. Input = 4096. Output = nb_classes(43).
+fc8W  = tf.Variable(tf.truncated_normal(shape, mean = 0, stddev = 0.01))
+fc8b  = tf.Variable(tf.zeros(nb_classes))
+
+logits = tf.matmul(fc7, fc8W) + fc8b
+probs = tf.nn.softmax(logits)
+
+
+# Training pipeline
+# rate = 0.001
+
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer()
+training_operation = optimizer.minimize(loss_operation, var_list=[fc8W, fc8b])
+
+# Evaluate
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
+
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+
+### Train your model here.
+### Calculate and report the accuracy on the training and validation set.
+### Once a final model architecture is selected,
+### the accuracy on the test set should be calculated and reported as well.
+### Feel free to use as many code cells as needed.
+
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+
+    print("Training...")
+    print()
+    for i in range(EPOCHS):
+        start_time = datetime.datetime.now()
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, int(num_examples), BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+
+        validation_accuracy = evaluate(X_valid, y_valid)
+        print("EPOCH {} in {} seconds ...".format(i + 1, (datetime.datetime.now() - start_time).seconds))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+        print()
+
+    saver.save(sess, './lenet')
+    print("Model saved")
+
